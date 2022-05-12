03bfa1c3a9b303e52b265c01dd98ccbf4593a206
hugosenari <hugosenari@gmail.com>
2022-05-12 12:11:46 -0300

    feat(nimpkgs): update lock

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."foliant-develop".dir   = "develop";
+  inputs."foliant-develop".owner = "nim-nix-pkgs";
+  inputs."foliant-develop".ref   = "master";
+  inputs."foliant-develop".repo  = "foliant";
+  inputs."foliant-develop".type  = "github";
+  inputs."foliant-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."foliant-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."foliant-master".dir   = "master";
+  inputs."foliant-master".owner = "nim-nix-pkgs";
+  inputs."foliant-master".ref   = "master";
+  inputs."foliant-master".repo  = "foliant";
+  inputs."foliant-master".type  = "github";
+  inputs."foliant-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."foliant-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
